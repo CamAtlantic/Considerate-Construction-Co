db8a680a2cbdad1713998761726d1c9c27dd75bc
Jai Bunnag <semonebunnag@gmail.com>
2016-10-18 18:27:16 +0100

    Added new colors, organized color matching.

--- Assets/Scripts/ColorManager.cs
+++ Assets/Scripts/ColorManager.cs
@@ -16,16 +16,16 @@ public class ColorManager : MonoBehaviour {
 	public static Color Clr_9;
 	public static Color Clr_10;
 
-	public Color Clr1_windows;
-	public Color Clr2;
-	public Color Clr3;
-	public Color Clr4;
-	public Color Clr5;
-	public Color Clr6;
-	public Color Clr7;
-	public Color Clr8;
-	public Color Clr9;
-	public Color Clr10;
+	public Color Clr1_base;
+	public Color Clr2_accent;
+	public Color Clr3_accent_2;
+	public Color Clr4_glass;
+	public Color Clr5_column;
+	public Color Clr6_column_foot;
+	public Color Clr7_roofing_1;
+	public Color Clr8_roofing_2;
+	public Color Clr9_inside;
+	public Color Clr10_base_back;
 	// Use this for initialization
 	void Start () {
 		
@@ -34,16 +34,16 @@ public class ColorManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		Clr_1 = Clr1_windows;
-		Clr_2 = Clr2;
-		Clr_3 = Clr3;
-		Clr_4 = Clr4;
-		Clr_5 = Clr5;
-		Clr_6 = Clr6;
-		Clr_7 = Clr7;
-		Clr_8 = Clr8;
-		Clr_9 = Clr9;
-		Clr_10 = Clr10;
+		Clr_1 = Clr1_base;
+		Clr_2 = Clr2_accent;
+		Clr_3 = Clr3_accent_2;
+		Clr_4 = Clr4_glass;
+		Clr_5 = Clr5_column;
+		Clr_6 = Clr6_column_foot;
+		Clr_7 = Clr7_roofing_1;
+		Clr_8 = Clr8_roofing_2;
+		Clr_9 = Clr9_inside;
+		Clr_10 = Clr10_base_back;
 		
 	}
 }
